2a6b362cd7adfa4b580457ae1f4506141d26b1fb
hwangyoungjae <dudwo56@gmail.com>
2020-09-10 15:43:59 +0900

    20

---
+++ example/10.5_asyncio/asyncio_echo_server_protocol.py
@@ -0,0 +1,14 @@
+# asyncio_echo_server_protocol.py
+import asyncio
+import logging
+import sys
+
+SERVER_ADDRESS = ("localhost", 10000)
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(name)s: %(message)s",
+    stream=sys.stderr,
+)
+log = logging.getLogger("main")
+event_loop = asyncio.get_event_loop()
